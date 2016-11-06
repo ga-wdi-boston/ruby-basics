69898b61b70d9ea99bfed6162c72dde8bbd36bb4
benjamski <benjadamski@gmail.com>
2016-11-06 11:51:38 -0500

    Use Rubocop preference for predicate methods
    - Made this change in all examples
    
    - Linter errors remain
     - Such as favoring `each` over `for`
     - Did not want to lose value of exmaples in linter appeasement.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -19,16 +19,16 @@ require 'pry'
 # end
 
 # Rub Case solution
-1.upto(max_num) do |i|
-  fizz = (i % 3).zero?
-  buzz = (i % 5).zero?
-  puts case i
-       when fizz && buzz then 'FizzBuzz'
-       when fizz then 'Fizz'
-       when buzz then 'Buzz'
-       else i
-       end
-end
+# 1.upto(max_num) do |i|
+#   fizz = (i % 3).zero?
+#   buzz = (i % 5).zero?
+#   puts case i
+#        when fizz && buzz then 'FizzBuzz'
+#        when fizz then 'Fizz'
+#        when buzz then 'Buzz'
+#        else i
+#        end
+# end
 
 # hash solution 1
 # def fizzbuzz(max_num)
@@ -40,11 +40,11 @@ end
 #   }
 #
 #   for i in 1.upto(max_num) do
-#     if i % 3 == 0 && i % 5 == 0
+#     if (i % 3).zero? && (i % 5).zero?
 #       fb[:fizzbuzz] << i
-#     elsif i % 3 == 0
+#     elsif (i % 3).zero?
 #       fb[:fizz] << i
-#     elsif i % 5 == 0
+#     elsif (i % 5).zero?
 #       fb[:buzz] << i
 #     else
 #       fb[:other] << i
@@ -56,25 +56,25 @@ end
 # hash solution 2
 # def fizzbuzz(max_num)
 #   fb = {
-#     "fizz" => [],
-#     "buzz" => [],
-#     "fizzbuzz" => [],
-#     "other" => []
+#     'fizz' => [],
+#     'buzz' => [],
+#     'fizzbuzz' => [],
+#     'other' => []
 #   }
-#
+
 #   for i in 1.upto(max_num) do
-#     if i % 3 == 0 && i % 5 == 0
-#       fb["fizzbuzz"] << i
-#     elsif i % 3 == 0
-#       fb["fizz"] << i
-#     elsif i % 5 == 0
-#       fb["buzz"] << i
+#     if (i % 3).zero? && (i % 5).zero?
+#       fb['fizzbuzz'] << i
+#     elsif (i % 3).zero?
+#       fb['fizz'] << i
+#     elsif (i % 5).zero?
+#       fb['buzz'] << i
 #     else
-#       fb["other"] << i
+#       fb['other'] << i
 #     end
 #   end
 #   puts fb
 # end
-#
+
 # binding.pry
 # ""
