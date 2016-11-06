a7ff0d5772c8e8a4aa80d59dc5f284a73973325d
benjamski <benjadamski@gmail.com>
2016-11-06 10:20:01 -0500

    Appease Rubo cop in solution for fizzbuzz
    - Add frozen string literal = true comment
    - Unable to fix following Rubocop warning:
     - Use`(i%3).0?` Instead of `i%3 ==0`
     - Changing results in unexepcted token errors

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,33 @@
+# frozen_string_literal: true
 require 'pry'
 
 # Let `max_num` be the number that you want to count to.
 
+# max_num = <number>
+
 # ruby solution
-# for i in 1.upto(max_num) do
-#   if i % 3 == 0 && i % 5 == 0
-#    puts "fizzbuzz"
-#   elsif i % 3 == 0
-#    puts "fizz"
-#   elsif i % 5 == 0
-#    puts "buzz"
-#   else
-#    puts "other"
-#   end
+each i 1.upto(max_num) do
+  if i % 3 == 0 && i % 5 == 0
+    puts 'fizzbuzz'
+  elsif i % 3 == 0
+    puts 'fizz'
+  elsif i % 5 == 0
+    puts 'buzz'
+  else
+    puts 'other'
+  end
+end
+
+# Rub Case solution
+# 1.upto(max_num) do |i|
+#   fizz = (i % 3 == 0)
+#   buzz = (i % 5 == 0)
+#   puts case
+#        when fizz && buzz then 'FizzBuzz'
+#        when fizz then 'Fizz'
+#        when buzz then 'Buzz'
+#        else i
+#        end
 # end
 
 # hash solution 1
